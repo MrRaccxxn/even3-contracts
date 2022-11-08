8e707cee5c8d2d95544c4007fd111c2e24bce47a
MrRaccxxn <ayrtonskt@gmail.com>
2022-11-08 15:27:17 -0400

    Base contracts

--- contracts/Diamond.sol
+++ contracts/Diamond.sol
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/******************************************************************************\
-* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
-* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
-*
-* Implementation of a diamond.
-/******************************************************************************/
-
 import { LibDiamond } from "./libraries/LibDiamond.sol";
 import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
 import { IDiamondLoupe } from  "./interfaces/IDiamondLoupe.sol";
@@ -17,9 +10,6 @@ import { IERC165} from "./interfaces/IERC165.sol";
 // When no function exists for function called
 error FunctionNotFound(bytes4 _functionSelector);
 
-// This is used in diamond constructor
-// more arguments are added to this struct
-// this avoids stack too deep errors
 struct DiamondArgs {
     address owner;
     address init;
---
+++ contracts/event3/legacy/EventFactory.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import './Event.sol';
+
+contract EventFactory {
+    uint256 counter;
+    Event public eventContract;
+
+    constructor() {
+        counter = 0;
+    }
+
+    mapping(uint256 => eventInfo) _events;
+
+    struct eventInfo {
+        uint256 eventId;
+        address owner;
+        string eventName;
+        address eventAddress;
+    }
+
+    event newEventCreated(uint256 eventId, address owner, string eventName, address eventAddress);
+
+    function createNewEvent(string memory eventName) public returns (address) {
+        address eventAddress = address(new Event(eventName, msg.sender));
+        eventInfo storage _eventInfo = _events[counter];
+        _eventInfo.eventId = counter;
+        _eventInfo.owner = msg.sender;
+        _eventInfo.eventName = eventName;
+        _eventInfo.eventAddress = eventAddress;
+
+        emit newEventCreated(counter, msg.sender, eventName, eventAddress);
+        counter++;
+
+        return eventAddress;
+    }
+
+    function getEventById(uint256 eventId)
+        public
+        view
+        returns (
+            uint256,
+            address,
+            string memory,
+            address
+        )
+    {
+        require(eventId >= 0 && eventId < counter, 'Please submit a valid Event Id');
+        eventInfo storage _eventInfo = _events[eventId];
+        return (_eventInfo.eventId, _eventInfo.owner, _eventInfo.eventName, _eventInfo.eventAddress);
+    }
+}
